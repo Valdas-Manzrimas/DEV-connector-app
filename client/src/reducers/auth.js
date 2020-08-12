acc7739510c33a36dbd60127665ea64c4ae195b0
Valdas-Manzrimas <vrajaviharidas@gmail.com>
2020-08-12 16:39:29 +0300

    User Login & Redirect

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -15,6 +17,7 @@ const initialState = {
 export default function(state = initialState, action) {
     const {type, payload} = action;
     switch (type) {
+
         case USER_LOADED: 
             return {
                 ...state, 
@@ -24,6 +27,7 @@ export default function(state = initialState, action) {
             }
 
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
                 ...state, 
@@ -32,7 +36,8 @@ export default function(state = initialState, action) {
                 loading: false
             }
         case REGISTER_FAIL:
-        case AUTH_ERROR: 
+        case AUTH_ERROR:
+        case LOGIN_FAIL: 
             localStorage.removeItem('token');
             return {
                 ...state, 
