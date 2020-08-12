9ef8640f0df680dadae2941d6587d12aedc1695d
Valdas-Manzrimas <vrajaviharidas@gmail.com>
2020-08-12 09:20:15 +0300

    Auth Reducer & Register Action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,11 +2,12 @@ import React, { Fragment, useState} from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -24,7 +25,7 @@ const Register = ({ setAlert }) => {
         if(password !== password2) {
             setAlert('Passwords do not match', 'danger')
         } else {
-            console.log('SUCCESS')
+            register({ name, email, password})
         }
     }
 
@@ -45,7 +46,7 @@ const Register = ({ setAlert }) => {
                     name='name'
                     value={name} 
                     onChange={e => onChange(e)} 
-                    required />
+                    />
             </div>
             <div   
                 className="form-group">
@@ -55,7 +56,6 @@ const Register = ({ setAlert }) => {
                     name="email"
                     value={email} 
                     onChange={e => onChange(e)}
-                    required
                     />
                     
                 <small className="form-text">
@@ -67,7 +67,6 @@ const Register = ({ setAlert }) => {
                     type="password" 
                     placeholder="Password" 
                     name="password"
-                    minLength="6" 
                     value={password} 
                     onChange={e => onChange(e)}
                     />
@@ -79,7 +78,7 @@ const Register = ({ setAlert }) => {
                     name="password2"
                     value={password2} 
                     onChange={e => onChange(e)}
-                    minLength="6" />
+                     />
             </div>
             <input 
                 type="submit" 
@@ -98,6 +97,9 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, 
+    { setAlert, register }
+    )(Register);
