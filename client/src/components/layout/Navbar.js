9348428dadf4fec0f0e46d9a576173381eec9474
Valdas-Manzrimas <vrajaviharidas@gmail.com>
2020-12-09 13:30:00 +0200

    Post Reducer, Action & Initial Component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,11 +12,13 @@ const Navbar = ({ auth: {isAuthenticated, loading }, logout}) => {
                     Developers
                 </Link>
             </li>
+            <li><Link to="/posts">Posts</Link></li>
             <li>
                 <Link to="/dashboard">
                     <i className="fas fa-user"></i>{' '}
                     <span className="hide-sm">Dashboard</span>
                 </Link>
+            
             </li>
             <li><a onClick={logout} href="#!">
                 <i className="fas fa-sign-out-alt"></i>{' '}
